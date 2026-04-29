bf44acc471ca3e55236010f603e376793d056591
Matthew Kenzie <matthew.kenzie@cern.ch>
2026-04-29 13:23:14 +0100

    update to run test snake workflow with different config options

--- utils.py
+++ utils.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python3
 
-from yaml import safe_load
+from yaml import safe_load, dump
 
 
 def load_config(config_path):
     """Load a YAML config file and return it as a dict."""
     with open(config_path) as f:
         return safe_load(f)
+
+def save_config(config_path, config):
+    with open(config_path, "w") as f:
+        dump(config, f)
+
